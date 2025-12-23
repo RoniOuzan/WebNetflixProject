089ec5070690e93d7da934aaac90e9f53cf84535
Tal Waldman <tal15.wald@gmail.com>
2025-12-23 10:36:21 +0200

    added project

---
+++ src/test/java/com/netflix/webnetflix/WebnetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.webnetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebnetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
