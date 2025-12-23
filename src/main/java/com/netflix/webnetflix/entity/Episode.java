2d1900077c56e2a72d5ba6af47745fe48e3e579f
RoniOuzan <roniouzan@gmail.com>
2025-12-23 10:58:14 +0200

    Filled series and episodes classes

--- src/main/java/com/netflix/webnetflix/entity/Episode.java
+++ src/main/java/com/netflix/webnetflix/entity/Episode.java
@@ -1,4 +1,38 @@
 package com.netflix.webnetflix.entity;
 
-public class Episode {
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+public class Episode implements Comparable<Episode> {
+    private int id;
+    private String name;
+    private String description;
+    private int episodeNumber;
+
+    public Episode(int id, int episodeNumber, String name, String description) {
+        this.id = id;
+        this.episodeNumber = episodeNumber;
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public int compareTo(Episode o) {
+        return Integer.compare(this.episodeNumber, o.episodeNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Episode episode = (Episode) o;
+        return this.id == episode.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.episodeNumber, this.name, this.description);
+    }
 }
--- src/main/java/com/netflix/webnetflix/entity/Series.java
+++ src/main/java/com/netflix/webnetflix/entity/Series.java
@@ -1,4 +1,39 @@
 package com.netflix.webnetflix.entity;
 
-public class Series {
+import lombok.Data;
+
+import java.util.List;
+import java.util.Objects;
+
+@Data
+public class Series implements Comparable<Series> {
+    private int id;
+    private String name;
+    private String description;
+    private List<Episode> episodes;
+
+    public Series(int id, String name, String description, List<Episode> episodes) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.episodes = episodes;
+    }
+
+    @Override
+    public int compareTo(Series o) {
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Series series = (Series) o;
+        return this.id == series.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.description, this.episodes);
+    }
 }
